7f666bfbb0628a05fe7c6008e2d7c55a4e0d5803
ADRIAN <lm_adrian_98@hotmail.com>
2019-07-11 07:24:50 -0500

    g

--- app/controllers/extra_controller.rb
+++ app/controllers/extra_controller.rb
@@ -1,4 +1,14 @@
 class ExtraController < ApplicationController
   def cotizacion
+  	@coti = Proyecto.all
   end
+  def detalle
+  	@detalle = Departamento.where(:tipo => "flat")
+  end
+   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_proyecto
+      @coti = Proyecto.find(params[:id])
+      @detalle = Departamento.find(params[:id])
+    end
 end
